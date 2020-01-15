93caeb6b945c3ded512440cec997d0ad793d0a8a
lucas-silva-dev <lucassilva.work@outlook.com>
2020-01-14 23:10:48 -0300

    Projeto acessibilidade

---
+++ src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+  secret: '2f0dada80d74a0be16b32434cfb2d838',
+  expiresIn: '20d',
+};
---
+++ src/config/database.js
@@ -0,0 +1,12 @@
+module.exports = {
+  dialect: 'postgres',
+  host: 'localhost',
+  username: 'postgres',
+  password: 'inprego',
+  database: 'acessibilidade',
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true,
+  },
+};
